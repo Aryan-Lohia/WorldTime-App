57a84ddd99c610b159750c57a38d006534a7a89c
Aryan Lohia <aryan_202100437@smit.smu.edu.in>
2022-08-04 00:15:47 +0530

    Update _defaultsettings.dart

--- lib/_defaultsettings.dart
+++ lib/_defaultsettings.dart
@@ -3,9 +3,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 String defaultarea="Asia/Kolkata";
 bool defaultannounce=false;
 String defaultnotify="Never";
+String defaulttasks="";
+
 Future initialize() async
 {
   final prefs = await SharedPreferences.getInstance();
+  defaulttasks=prefs.get("tasks") as String;
   defaultarea= prefs.get("area") as String;
   defaultnotify= prefs.get("notify") as String;
   defaultannounce= prefs.get("announce") as bool;
@@ -13,6 +16,7 @@ Future initialize() async
 Future setpreferences() async
 {
   final prefs = await SharedPreferences.getInstance();
+  prefs.setString("tasks", defaulttasks);
   prefs.setString("area", defaultarea);
   prefs.setString("notify", defaultnotify);
   prefs.setBool("announce", defaultannounce);
